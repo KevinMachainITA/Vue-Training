4c2e83fa0070b6cb9bf8114417eba4361085ae6e
KevinMachainITA <20151626@aguascalientes.tecnm.mx>
2024-09-20 12:44:06 -0600

    feat (App.vue): :sparkles: add number to list

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,9 @@
 
   const name = "Vue 3";
 
-  let counter = ref(0);
+  const counter = ref(0);
+
+  const numberList = ref([]);
 
   const increase = () => {
     counter.value++;
@@ -16,6 +18,41 @@
   const reset = () => {
     counter.value = 0;
   }
+
+  /**
+   * Adds a number to the `numberList` array if it doesn't already exist.
+   * If the number is found in the list, it logs a message to the console.
+   * 
+   * @param {number} number - The number to add to the list.
+   * @returns {void}
+  */
+  const add = (number) => {
+    const index = numberList.value.findIndex(num => num === number);
+
+    if(index === -1){
+      numberList.value.push(number);
+    } else {
+      console.log("this number is in the list");
+    }
+  }
+
+  /**
+   * Determines if the "Add" button should be available.
+   * If the current `counter` value is found in the `numberList`, the button will be disabled.
+   * 
+   * @returns {boolean} - Returns `true` if the button should be disabled, `false` otherwise.
+   */
+  const buttonAvailable = computed(() => {
+    const numberSearch = numberList.value.find(num => num === counter.value);
+
+    if(numberSearch === 0){
+      return true;
+    }
+
+    //If currently counter number is within array, the add button will be disabled
+    return numberSearch ? true : false;
+
+  });
 
   const classCounter = computed(() => {
     if(counter.value === 0){
@@ -33,12 +70,21 @@
   <main class="container">
     <h1>!HI {{ name.toUpperCase() }}!</h1>
     <h2 :class="classCounter">{{counter}}</h2>
+
     <div class="main-buttons">
       <button @click="increase()" class="button">Increase</button>
       <button @click="reset()" class="button">Reset</button>
       <button @click="decrease()" class="button">Decrease</button>
+      <button @click="add(counter)" :class="buttonAvailable ? 'button-disabled' : 'button'" :disabled="buttonAvailable">Add</button>
     </div>
+
   </main>
+
+  <h3 class="title-list">Number list</h3>
+  <ul class="list-numbers">
+    <li v-for="(num, index) in numberList" :key="index">{{num}}</li>
+  </ul>
+
 </template>
 
 <style>
@@ -62,12 +108,17 @@
   }
 
   .main-buttons{
-    display: flex;
+    display: grid;
+    grid-template-columns: repeat(3, 1fr); /* 3 columns */
+    gap: 1rem; 
     width: 100%;
     justify-content: center;
     align-items: center;
-    gap: .5rem;
     margin-top: 1rem;
+  }
+
+  .main-buttons button:nth-child(4){
+    grid-column: 2 / 3; /* Place in the second column */
   }
 
   .button {
@@ -79,6 +130,7 @@
     text-decoration: none;
     display: inline-block;
     font-size: 16px;
+    cursor: pointer;
   }
 
   .button:hover{
@@ -95,5 +147,30 @@
 
   .zero {
     color: #3f3f3f
+  }
+
+  .title-list {
+    margin: 2rem 0;
+    color: #fff;
+    text-transform: uppercase;
+  }
+
+  .list-numbers {
+    display: grid;
+    grid-template-columns: repeat(7, 1fr); /* 8 columns */
+    gap: 1rem;
+  }
+
+  .button-disabled {
+    background-color: #04AA6D; /* Green */
+    border: none;
+    color: white;
+    padding: 15px 32px;
+    text-align: center;
+    text-decoration: none;
+    display: inline-block;
+    font-size: 16px;
+    opacity: 0.6;
+    cursor: not-allowed;
   }
 </style>
